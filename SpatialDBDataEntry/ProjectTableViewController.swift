3cf553050918909d5298a9ce3d226b74c1e61126
karanseq <karanseq@gmail.com>
2017-10-01 19:01:07 -0600

    Added: Project and site delete.

--- SpatialDBDataEntry/ProjectTableViewController.swift
+++ SpatialDBDataEntry/ProjectTableViewController.swift
@@ -59,17 +59,18 @@ class ProjectTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+
+    // Support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            // Delete the project
+            projects.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
--- SpatialDBDataEntry/SiteTableViewController.swift
+++ SpatialDBDataEntry/SiteTableViewController.swift
@@ -70,17 +70,18 @@ class SiteTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+    // Support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            // Delete the site
+            project!.sites.remove(at: indexPath.row)
+            sites.remove(at: indexPath.row)
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
