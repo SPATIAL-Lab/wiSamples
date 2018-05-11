c41963f96963c7ee7ee7b6abe20dbafc956931b3
Gabriel Bowen <gabe.bowen@utah.edu>
2018-05-10 21:06:14 -0600

    Update PackViewController.swift
    Initial draft code to be integrated w/ storyboard

--- SpatialDBDataEntry/PackViewController.swift
+++ SpatialDBDataEntry/PackViewController.swift
@@ -1,1 +1,145 @@
 
+//
+//  PackViewController.swift
+//  SpatialDBDataEntry
+//
+//  Created by Gabe Bowen on 10/27/17.
+//  Copyright © 2018 University of Utah. All rights reserved.
+//
+
+import Mapbox
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MGLMapViewDelegate {  
+    let locationManager = CLLocationManager()
+    
+    var progressView: UIProgressView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let project = DataManager.shared.projects[projectIndex]
+        MapViewController.setStyle(index: project.defaultMap)
+        mapView.delegate = self
+      
+        if Reachability.isConnectedToNetwork() {
+          // Create a UISegmentedControl to toggle between map styles
+          let styleToggle = UISegmentedControl(items: ["Streets", "Satellite"])
+          styleToggle.translatesAutoresizingMaskIntoConstraints = false
+          styleToggle.backgroundColor = UIColor.white
+          styleToggle.selectedSegmentIndex = 0
+          view.insertSubview(styleToggle, aboveSubview: mapView)
+          styleToggle.addTarget(self, action: #selector(MapViewController.changeStyle(sender:)), for: .valueChanged)
+
+          // Configure autolayout constraints for the UISegmentedControl to align
+          // at the bottom of the map view and above the Mapbox logo and attribution
+          NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|-40-[styleToggle]-40-|", options: [], metrics: nil, views: ["styleToggle" : styleToggle]))
+          NSLayoutConstraint.activate([NSLayoutConstraint(item: styleToggle, attribute: .bottom, relatedBy: .equal, toItem: mapView.logoView, attribute: .top, multiplier: 1, constant: -20)])
+        }
+      
+        // Initialize location
+        // Request location usage
+        locationManager.requestWhenInUseAuthorization()
+        
+        // Setup location services
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delayBeforeInitialZoom)) {
+             mapView.setCenter(locationManager.lastUpdatedLocation.coordinate, zoomLevel: 10, animated: true)
+        }
+        
+        // Setup offline pack notification handlers.
+        NotificationCenter.default.addObserver(self, selector: #selector(offlinePackProgressDidChange), name: NSNotification.Name.MGLOfflinePackProgressChanged, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(offlinePackDidReceiveError), name: NSNotification.Name.MGLOfflinePackError, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(offlinePackDidReceiveMaximumAllowedMapboxTiles), name: NSNotification.Name.MGLOfflinePackMaximumMapboxTilesReached, object: nil)
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+        // Start downloading tiles and resources for z13-16.
+        startOfflinePackDownload()
+    }
+    
+    deinit {
+        // Remove offline pack observers.
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    func startOfflinePackDownload() {
+        // Create a region that includes the current viewport and any tiles needed to view it when zoomed further in.
+        // Because tile count grows exponentially with the maximum zoom level, you should be conservative with your `toZoomLevel` setting.
+        let region = MGLTilePyramidOfflineRegion(styleURL: mapView.styleURL, bounds: mapView.visibleCoordinateBounds, fromZoomLevel: mapView.zoomLevel, toZoomLevel: mapView.zoomLevel + 6)
+        
+        // Store some data for identification purposes alongside the downloaded resources.
+        let userInfo = ["name": "My Offline Pack"]
+        let context = NSKeyedArchiver.archivedData(withRootObject: userInfo)
+       
+        // Create and register an offline pack with the shared offline storage object.
+        
+        MGLOfflineStorage.shared.addPack(for: region, withContext: context) { (pack, error) in
+            guard error == nil else {
+                // The pack couldn’t be created for some reason.
+                print("Error: \(error?.localizedDescription ?? "unknown error")")
+                return
+            }
+            
+            // Start downloading.
+            pack!.resume()
+        }
+        
+    }
+    
+    // MARK: - MGLOfflinePack notification handlers
+    
+    @objc func offlinePackProgressDidChange(notification: NSNotification) {
+        // Get the offline pack this notification is regarding,
+        // and the associated user info for the pack; in this case, `name = My Offline Pack`
+        if let pack = notification.object as? MGLOfflinePack,
+            let userInfo = NSKeyedUnarchiver.unarchiveObject(with: pack.context) as? [String: String] {
+            let progress = pack.progress
+            // or notification.userInfo![MGLOfflinePackProgressUserInfoKey]!.MGLOfflinePackProgressValue
+            let completedResources = progress.countOfResourcesCompleted
+            let expectedResources = progress.countOfResourcesExpected
+            
+            // Calculate current progress percentage.
+            let progressPercentage = Float(completedResources) / Float(expectedResources)
+            
+            // Setup the progress bar.
+            if progressView == nil {
+                progressView = UIProgressView(progressViewStyle: .default)
+                let frame = view.bounds.size
+                progressView.frame = CGRect(x: frame.width / 4, y: frame.height * 0.75, width: frame.width / 2, height: 10)
+                view.addSubview(progressView)
+            }
+            
+            progressView.progress = progressPercentage
+            
+            // If this pack has finished, print its size and resource count.
+            if completedResources == expectedResources {
+                let byteCount = ByteCountFormatter.string(fromByteCount: Int64(pack.progress.countOfBytesCompleted), countStyle: ByteCountFormatter.CountStyle.memory)
+                print("Offline pack “\(userInfo["name"] ?? "unknown")” completed: \(byteCount), \(completedResources) resources")
+            } else {
+                // Otherwise, print download/verification progress.
+                print("Offline pack “\(userInfo["name"] ?? "unknown")” has \(completedResources) of \(expectedResources) resources — \(progressPercentage * 100)%.")
+            }
+        }
+    }
+    
+    @objc func offlinePackDidReceiveError(notification: NSNotification) {
+        if let pack = notification.object as? MGLOfflinePack,
+            let userInfo = NSKeyedUnarchiver.unarchiveObject(with: pack.context) as? [String: String],
+            let error = notification.userInfo?[MGLOfflinePackUserInfoKey.error] as? NSError {
+            print("Offline pack “\(userInfo["name"] ?? "unknown")” received error: \(error.localizedFailureReason ?? "unknown error")")
+        }
+    }
+    
+    @objc func offlinePackDidReceiveMaximumAllowedMapboxTiles(notification: NSNotification) {
+        if let pack = notification.object as? MGLOfflinePack,
+            let userInfo = NSKeyedUnarchiver.unarchiveObject(with: pack.context) as? [String: String],
+            let maximumCount = (notification.userInfo?[MGLOfflinePackUserInfoKey.maximumCount] as AnyObject).uint64Value {
+            print("Offline pack “\(userInfo["name"] ?? "unknown")” reached limit of \(maximumCount) tiles.")
+        }
+    }
+    
+}
